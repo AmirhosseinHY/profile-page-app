3de46a5b6b6bc3fbc47c7dc7a4afe08cee9fd8ee
AmirhosseinHY <amirhossein.homayouni@gmail.com>
2025-07-13 19:53:49 +0330

    add localization files

---
+++ lib/l10n/app_localizations_fa.dart
@@ -0,0 +1,50 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Persian (`fa`).
+class AppLocalizationsFa extends AppLocalizations {
+  AppLocalizationsFa([String locale = 'fa']) : super(locale);
+
+  @override
+  String get profileTitle => 'صفحه پروفایل';
+
+  @override
+  String get name => 'برایس سرافین';
+
+  @override
+  String get job => 'طراحی و تولید محصول';
+
+  @override
+  String get location => 'پاریس، فرانسه';
+
+  @override
+  String get summary =>
+      'من یک کامپیوتر باز جوان و مشتاق، طراح آزادکار و عاشق استقلال هستم، تجربه زیادی در پروژه های گرافیکی دارم و همیشه تمام تلاشم را می کنم تا شما را به موفقیت برسانم.';
+
+  @override
+  String get selectedLanguage => 'انتخاب زبان';
+
+  @override
+  String get enLanguage => 'انگلیسی';
+
+  @override
+  String get faLanguage => 'فارسی';
+
+  @override
+  String get skills => 'مهارت ها';
+
+  @override
+  String get personalInformation => 'اطلاعات فردی';
+
+  @override
+  String get email => 'ایمیل';
+
+  @override
+  String get password => 'رمز عبور';
+
+  @override
+  String get save => 'ذخیره';
+}
